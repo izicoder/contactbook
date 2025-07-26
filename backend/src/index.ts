9748d5931a6c58b55b86369fb63ecd87d32df37e
izicoder <tronnorskyrim@gmail.com>
2025-07-27 02:40:48 +0500

    server setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,5 +6,10 @@ app.use(cors())
 app.use(express.json())
 
 app.get("/contact", async (req, res) => {
+    console.log(`request from ${req.ip}`)
+    console.log(req.headers)
     res.json({ msg: "hello" })
 })
+
+const port = 9999
+app.listen(port, () => console.log(`running on ${port}`))
